9f19fd6d5831d6dfe6e29c4ec17fd6116373b7b0
hehe <hehe@gmail.com>
2025-01-28 08:47:36 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by froehly jean-baptiste on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
